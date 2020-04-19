1a28e52fc4d72f02f0c7a55c2701c64a3f4fde45
sunjian126 <sunjian11@jd.com>
2020-04-19 23:57:19 +0800

    support linux

--- build.rs
+++ build.rs
@@ -1,5 +1,19 @@
 fn main() {
-    println!("cargo:rustc-link-lib=dylib=omp");
+    if get_os_type() == "linux" {
+        println!("cargo:rustc-link-lib=dylib=omp");
+    }
     println!("cargo:rustc-link-lib=dylib=faiss");
     cpp_build::Config::new().build("src/lib.rs");
+}
+
+fn get_os_type() -> &'static str {
+    if cfg!(target_os = "windows") {
+        return "windows";
+    } else if cfg!(target_os = "linux") {
+        return "linux";
+    } else if cfg!(target_os = "macos") {
+        return "macos";
+    } else {
+        return "unknown_os";
+    }
 }
