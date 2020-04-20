3c913a9b8d5e685703ef06c34b7354dcc746f729
sunjian126 <sunjian11@jd.com>
2020-04-20 23:28:07 +0800

    up

--- build.rs
+++ build.rs
@@ -3,15 +3,13 @@ fn main() {
         println!("cargo:rustc-link-lib=dylib=omp");
         println!("cargo:rustc-link-lib=dylib=faiss");
     } else {
-        // println!("cargo:rustc-link-lib=dylib=stdc++");
+        println!("cargo:rustc-link-lib=dylib=stdc++");
         println!("cargo:rustc-link-lib=dylib=faiss");
         println!("cargo:rustc-link-lib=dylib=gomp");
         println!("cargo:rustc-link-lib=dylib=blas");
         println!("cargo:rustc-link-lib=dylib=lapack");
     }
-    cpp_build::Config::new()
-        .cpp_link_stdlib(Some("stdc++"))
-        .build("src/lib.rs");
+    cpp_build::Config::new().build("src/lib.rs");
 }
 
 fn get_os_type() -> &'static str {
