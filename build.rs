efda6794a1d796e05aa23c6b9ad6da5b34a7a628
sunjian126 <sunjian11@jd.com>
2020-04-21 00:28:55 +0800

    up

--- build.rs
+++ build.rs
@@ -3,9 +3,8 @@ fn main() {
         println!("cargo:rustc-link-lib=dylib=omp");
         println!("cargo:rustc-link-lib=dylib=faiss");
     } else {
-        println!("cargo:rustc-flags=-l dylib=stdc++");
-
-        println!("cargo:rustc-link-lib=dylib=faiss");
+        println!("cargo:rustc-link-lib=dylib=stdc++");
+        println!("cargo:rustc-link-lib=faiss");
         println!("cargo:rustc-link-lib=dylib=gomp");
         println!("cargo:rustc-link-lib=dylib=blas");
         println!("cargo:rustc-link-lib=dylib=lapack");
