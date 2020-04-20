80f0631c6368c6c380e2ace75b6d9706a21aa4b5
sunjian126 <sunjian11@jd.com>
2020-04-21 00:36:16 +0800

    up

--- build.rs
+++ build.rs
@@ -3,12 +3,13 @@ fn main() {
         println!("cargo:rustc-link-lib=dylib=omp");
         println!("cargo:rustc-link-lib=dylib=faiss");
     } else {
-        // println!("cargo:rustc-link-lib=stdc++");
-        // println!("cargo:rustc-link-lib=faiss");
-        // println!("cargo:rustc-link-lib=dylib=gomp");
-        // println!("cargo:rustc-link-lib=dylib=blas");
-        // println!("cargo:rustc-link-lib=dylib=lapack");
+        println!("cargo:rustc-link-lib=dylib=stdc++");
+        println!("cargo:rustc-link-lib=dylib=faiss");
+        println!("cargo:rustc-link-lib=dylib=gomp");
+        println!("cargo:rustc-link-lib=dylib=blas");
+        println!("cargo:rustc-link-lib=dylib=lapack");
     }
+    // export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:/home/rust-faiss/faiss
     cpp_build::Config::new().build("src/lib.rs");
 }
 
