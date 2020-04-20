9445be6174dca37332a379c8650e7945e3f104da
sunjian126 <sunjian11@jd.com>
2020-04-20 23:54:34 +0800

    up

--- build.rs
+++ build.rs
@@ -11,7 +11,7 @@ fn main() {
         println!("cargo:rustc-link-lib=dylib=lapack");
     }
     cpp_build::Config::new()
-        .cpp_set_stdlib(Some("stdc++"))
+        .cpp_link_stdlib(Some("c++11"))
         .build("src/lib.rs");
 }
 
