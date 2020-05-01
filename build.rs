ce62b5659882ad233c5fe3a96896ad958b574111
sunjian126 <sunjian11@jd.com>
2020-05-02 01:28:46 +0800

    static compile with faiss

--- build.rs
+++ build.rs
@@ -1,9 +1,21 @@
 fn main() {
     if get_os_type() == "macos" {
-        println!("cargo:rustc-link-lib=dylib=omp");
-        println!("cargo:rustc-link-lib=dylib=faiss");
+        println!("cargo:rustc-link-search=faiss/");
+        println!("cargo:rustc-link-search=/usr/local/lib");
+        println!("cargo:rustc-link-search=/usr/lib");
+        if let Ok(paths) = std::env::var("LD_LIBRARY_PATH") {
+            for path in paths.split(":") {
+                if path != "" {
+                    println!("cargo:rustc-link-search={}", path);
+                }
+            }
+        };
+        println!("cargo:rustc-link-lib=static=omp");
+        println!("cargo:rustc-link-lib=static=faiss");
     } else {
         println!("cargo:rustc-link-search=faiss/");
+        println!("cargo:rustc-link-search=/usr/local/lib");
+        println!("cargo:rustc-link-search=/usr/lib");
         if let Ok(paths) = std::env::var("LD_LIBRARY_PATH") {
             for path in paths.split(":") {
                 if path != "" {
@@ -21,13 +33,11 @@ fn main() {
 }
 
 fn get_os_type() -> &'static str {
-    if cfg!(target_os = "windows") {
-        return "windows";
-    } else if cfg!(target_os = "linux") {
+    if cfg!(target_os = "linux") {
         return "linux";
     } else if cfg!(target_os = "macos") {
         return "macos";
     } else {
-        return "unknown_os";
+        panic!("unknow os type");
     }
 }
